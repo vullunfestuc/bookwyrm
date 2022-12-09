a2125d0627a795b1666670dfdf0863c26d59e09c
vullunfestuc <damia.felip@gmail.com>
2022-12-09 07:52:12 +0100

    Adding failed merged.

--- bookwyrm/connectors/settings.py
+++ bookwyrm/connectors/settings.py
@@ -1,7 +1,3 @@
 """ settings book data connectors """
 
-<<<<<<< HEAD
 CONNECTORS = ["openlibrary", "inventaire", "bookwyrm_connector", "atenacat"]
-=======
-CONNECTORS = ["openlibrary", "inventaire", "bookwyrm_connector","atenacat"]
->>>>>>> 4dcd79516e5b5e39701d60814da58421cf9306e5
